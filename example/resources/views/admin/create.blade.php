6b2c160df5d1eaac461f4f80c04c926ca94916a9
dmytro <andrushchenkodp@gmail.com>
2023-04-11 23:26:59 +0300

    ggg

--- example/resources/views/admin/create.blade.php
+++ example/resources/views/admin/create.blade.php
@@ -6,11 +6,11 @@
             <label for="exampleFormControlInput1" class="form-label">Title</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="title">
         </div>
-        <div class="mb-lg-3">
+        <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Category</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="category">
         </div>
-        <div class="mb-lg-3">
+        <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Tag</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="tag">
         </div>
@@ -18,7 +18,7 @@
             <label for="exampleFormControlTextarea1" class="form-label">Description</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
-        <div class="mb-lg-3">
+        <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Image</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="image">
         </div>
